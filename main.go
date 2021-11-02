f718a744e0553cefa2c2a0280c2535355c2072e1
Jasy <972408670@qq.com>
2021-11-02 11:59:30 +0800

    美化文章列表页

--- main.go
+++ main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
 	"jasy/goblog/app/http/middlewares"
 	"jasy/goblog/bootstrap"
-	"jasy/goblog/pkg/database"
 	"net/http"
 )
 
-var router *mux.Router
-var db *sql.DB
-
-
 func main() {
 
-	database.Initialize()
-	db = database.DB
-
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
 
 	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
