69cc8452b542fb8a03b98e9052db47aac8de5f38
Jasy <972408670@qq.com>
2021-11-02 19:06:54 +0800

    美化文章表单页

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -25,8 +25,8 @@ func GetAll() ([]Article, error) {
 }
 
 // Create 创建文章，通过 article.ID 来判断是否创建成功
-func (article Article) Create() (err error) {
-	if err = model.DB.Create(&article).Error; err != nil {
+func (article *Article) Create() (err error) {
+	if err = model.DB.Create(article).Error; err != nil {
 		logger.LogError(err)
 		return err
 	}
@@ -35,8 +35,8 @@ func (article Article) Create() (err error) {
 }
 
 // Update 更新文章
-func (article Article) Update() (rowsAffected int64, err error) {
-	result := model.DB.Save(&article)
+func (article *Article) Update() (rowsAffected int64, err error) {
+	result := model.DB.Save(article)
 	if err = result.Error; err != nil {
 		logger.LogError(err)
 		return 0, err
