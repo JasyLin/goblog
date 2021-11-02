80d568fdd48d728c5fb4be993a958606c7372b50
Jasy <972408670@qq.com>
2021-11-02 16:09:41 +0800

    文章内容页

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -42,14 +42,25 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request)  {
 			fmt.Fprint(w, "500 服务器内部错误")
 		}
 	} else {
+		viewDir := "resources/views"
+
+		// 4.1 所有布局模板文件 Slice
+		files, err := filepath.Glob(viewDir + "/layouts/*.gohtml")
+		logger.LogError(err)
+
+		// 4.2 在 Slice 里新增我们的目标文件
+		newFiles := append(files, viewDir+"/articles/show.gohtml")
+
+		// 4.3 解析模板文件
 		tmpl, err := template.New("show.gohtml").
 			Funcs(template.FuncMap{
 				"RouteName2URL": route.Name2URL,
 				"Int64ToString": types.Int64ToString,
-			}).ParseFiles("resources/views/articles/show.gohtml")
+			}).ParseFiles(newFiles...)
 		logger.LogError(err)
 
-		tmpl.Execute(w, _article)
+		// 4.4 渲染模板，将所有文章的数据传输进去
+		tmpl.ExecuteTemplate(w, "app", _article)
 	}
 }
 
