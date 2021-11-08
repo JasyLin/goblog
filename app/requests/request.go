bbfda838ceaac4b1149fc3748d083e6450f0fda2
Jasy <972408670@qq.com>
2021-11-08 17:07:02 +0800

    test

---
+++ app/requests/request.go
@@ -0,0 +1,49 @@
+package requests
+
+import (
+	"errors"
+	"fmt"
+	"github.com/thedevsaddam/govalidator"
+	"jasy/goblog/pkg/model"
+	"strings"
+)
+
+func init() {
+	govalidator.AddCustomRule("not_exists", func(field string, rule string, message string, value interface{}) error {
+		rng := strings.Split(strings.TrimPrefix(rule, "not_exists"), ",")
+		tableName := rng[0]
+		dbFiled := rng[1]
+		val := value.(string)
+
+		var count int64
+		model.DB.Table(tableName).Where(dbFiled+" = ?", val).Count(&count)
+		if count != 0 {
+			if message != "" {
+				return errors.New(message)
+			}
+
+			return fmt.Errorf("%v 已被占用", val)
+		}
+		return nil
+	})
+}
+
+//func init() {
+//	govalidator.AddCustomRule("not_exists", func(field string, rule string, message string, value interface{}) error {
+//		rng := strings.Split(strings.TrimPrefix(rule, "not_exists:"), ",")
+//		tableName := rng[0]
+//		dbFiled := rng[1]
+//		val := value.(string)
+//
+//		var count int64
+//		model.DB.Table(tableName).Where(dbFiled+" = ?", val).Count(&count)
+//		if count != 0 {
+//			if message != "" {
+//				return errors.New(message)
+//			}
+//
+//			return fmt.Errorf("%v 已被占用", val)
+//		}
+//		return nil
+//	})
+//}
