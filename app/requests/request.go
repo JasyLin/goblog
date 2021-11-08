9360c2a40446eae26b73160072c9101900b6cba8
Jasy <972408670@qq.com>
2021-11-08 17:08:18 +0800

    确保用户名和邮箱唯一

--- app/requests/request.go
+++ app/requests/request.go
@@ -10,7 +10,7 @@ import (
 
 func init() {
 	govalidator.AddCustomRule("not_exists", func(field string, rule string, message string, value interface{}) error {
-		rng := strings.Split(strings.TrimPrefix(rule, "not_exists"), ",")
+		rng := strings.Split(strings.TrimPrefix(rule, "not_exists:"), ",")
 		tableName := rng[0]
 		dbFiled := rng[1]
 		val := value.(string)
@@ -28,22 +28,3 @@ func init() {
 	})
 }
 
-//func init() {
-//	govalidator.AddCustomRule("not_exists", func(field string, rule string, message string, value interface{}) error {
-//		rng := strings.Split(strings.TrimPrefix(rule, "not_exists:"), ",")
-//		tableName := rng[0]
-//		dbFiled := rng[1]
-//		val := value.(string)
-//
-//		var count int64
-//		model.DB.Table(tableName).Where(dbFiled+" = ?", val).Count(&count)
-//		if count != 0 {
-//			if message != "" {
-//				return errors.New(message)
-//			}
-//
-//			return fmt.Errorf("%v 已被占用", val)
-//		}
-//		return nil
-//	})
-//}
