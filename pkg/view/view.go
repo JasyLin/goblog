830d437cd193f330b39c3cec820dc22a7fd0d142
Jasy <972408670@qq.com>
2021-11-09 17:41:07 +0800

    登录状态

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -3,6 +3,7 @@ package view
 import (
 	"html/template"
 	"io"
+	"jasy/goblog/pkg/auth"
 	"jasy/goblog/pkg/logger"
 	"jasy/goblog/pkg/route"
 	"path/filepath"
@@ -13,17 +14,34 @@ import (
 type D map[string]interface{}
 
 // Render 渲染通用视图
-func Render(w io.Writer, data interface{}, tplFiles ...string) {
+func Render(w io.Writer, data D, tplFiles ...string) {
 	RenderTemplate(w, "app", data, tplFiles...)
 }
 
 // RenderSimple 渲染简单的视图
-func RenderSimple(w io.Writer, data interface{}, tplFiles ...string) {
+func RenderSimple(w io.Writer, data D, tplFiles ...string) {
 	RenderTemplate(w, "simple", data, tplFiles...)
 }
 
 // RenderTemplate 渲染视图
-func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...string) {
+func RenderTemplate(w io.Writer, name string, data D, tplFiles ...string) {
+
+	data["isLogined"] = auth.Check()
+
+	allFiles := getTemplateFiles(tplFiles...)
+
+	// 5 解析所有模板文件
+	tmpl, err := template.New("").
+		Funcs(template.FuncMap{
+			"RouteName2URL": route.Name2URL,
+		}).ParseFiles(allFiles...)
+	logger.LogError(err)
+
+	// 6 渲染模板
+	tmpl.ExecuteTemplate(w, name, data)
+}
+
+func getTemplateFiles(tplFiles ...string) []string {
 	// 1 设置模板相对路径
 	viewDir := "resources/views/"
 
@@ -37,15 +55,5 @@ func RenderTemplate(w io.Writer, name string, data interface{}, tplFiles ...stri
 	logger.LogError(err)
 
 	// 4. 合并所有文件
-	allFiles := append(layoutFiles, tplFiles...)
-
-	// 5 解析所有模板文件
-	tmpl, err := template.New("").
-		Funcs(template.FuncMap{
-			"RouteName2URL": route.Name2URL,
-		}).ParseFiles(allFiles...)
-	logger.LogError(err)
-
-	// 6 渲染模板
-	tmpl.ExecuteTemplate(w, name, data)
+	return append(layoutFiles, tplFiles...)
 }
