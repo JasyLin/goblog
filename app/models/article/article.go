da89571461f2ed606fd993ad7bed93de8cf3c3ad
Jasy <972408670@qq.com>
2021-11-12 17:31:33 +0800

    重构文章表单验证

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -7,8 +7,8 @@ import (
 
 type Article struct {
 	models.BaseModel
-	Title string
-	Body  string
+	Title string `gorm:"type:varchar(255);not null;" valid:"title"`
+	Body  string `gorm:"type:longtext;not null;" valid:"body"`
 }
 
 func (a Article) Link() string {
